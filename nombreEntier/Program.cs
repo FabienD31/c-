435d541ed89056b56019137f383056b56e051203
desnoues <fabien.desnoues31@gmail.com>
2020-09-28 10:37:14 +0200

    nombre entier

---
+++ nombreEntier/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace nombre_entiers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = int.MaxValue;
+            long y = long.MaxValue;
+            short z = short.MaxValue;
+
+            ulong a = (ulong)x + (ulong)y + (ulong)z;
+            Console.WriteLine(a);
+        }
+    }
+}
+
